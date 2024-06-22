c38214550930e91b0a09a9ff95c3421daa1a1aec
Dillon <dillon.20221517@iit.ac.lk>
2024-06-22 14:44:42 +0530

    Update landing page designby adding BG

--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import LandingSection from "./components/LandingSection";
 export default function page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden ">
-      <div className="max-w-7xl mx-auto p-5  ">
+      <div className="max-w-7xl mx-auto p-5 dark:bg-grid-white/[0.2] bg-grid-black/[0.2] ">
         <Navbar />
         <LandingSection />
       </div>
