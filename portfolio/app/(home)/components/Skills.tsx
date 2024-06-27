ed0eb0be3ac91a3b613a41e1826d0b8944a2d4cd
Dillon <dillon.20221517@iit.ac.lk>
2024-06-27 19:13:43 +0530

    Add Skills component to home page

---
+++ portfolio/app/(home)/components/Skills.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold group-hover:text-purple-500 transition-all ">
+        Skills 📮
+      </h1>
+      <div className="w-40 h-2 bg-green-500 rounded-full"></div>
+      <div className="w-40 h-2 bg-blue-500 rounded-full translate-x-2 "></div>
+    </div>
+  );
+}
--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import LandingSection from "./components/LandingSection";
+import Skills from "./components/Skills";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden ">
       {/* this is the bg grid box line  */}
-      <div className="dark:bg-grid-white/[0.06] bg-grid-black/[0.2] ">
+      <div className="dark:bg-grid-white/[0.06] bg-grid-black/[0.2] relative ">
         <div className="max-w-7xl mx-auto p-5 ">
           <Navbar />
           <LandingSection />
         </div>
+        <div className="h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
+      </div>
+      <div className="max-w-7xl mx-auto p-5 ">
+        <Skills />
       </div>
     </div>
   );
